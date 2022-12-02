3c5b2a1757b2eaf71eb5d63dad592e4fff7ef5f0
Dedan Irungu <dedanirungu@gmail.com>
2022-12-02 09:39:48 +0300

    description1
    description1

--- description/index.vue
+++ description/index.vue
@@ -4,7 +4,7 @@
 <script>
   export default {
     props: {
-    name: "description",
+    name: "description1",
       article: {
         type: String,
         default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
